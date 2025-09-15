6b88889b6c0931ae290da641b174ff2c96358b95
dilly <dilly.chen@titansoft.com.sg>
2025-09-15 17:54:53 +0800

    feat: add vite-plugin-single-spa for microfrontend support; update package.json and vite.config.ts to include new plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import singleSpa from 'vite-plugin-single-spa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,10 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    singleSpa({
+      type: 'root',
+      imo: false,
+    }),
   ],
   resolve: {
     alias: {
